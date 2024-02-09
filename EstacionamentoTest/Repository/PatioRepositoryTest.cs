1edb90c7c0b9a64226c120a2aa0e196b4b604322
Luiz Guilherme Barbieri <barbieri_lgb@hotmail.com>
2024-02-09 17:23:58 -0300

    feat: Adiciona testes unitarios PatioRepository e RegistroVeiculoRepository

---
+++ EstacionamentoTest/Repository/PatioRepositoryTest.cs
@@ -0,0 +1,104 @@
+﻿using EstacionamentoV2.Context;
+using EstacionamentoV2.Model;
+using EstacionamentoV2.Repository;
+
+namespace EstacionamentoTest.Repository;
+
+public class PatioRepositoryTest
+{
+    private readonly EstacionamentoContext _context;
+    private readonly PatioRepository _repository;
+
+    public PatioRepositoryTest()
+    {
+        _context = GeradorContextoEmMemoria.GerarContexto();
+        _repository = new PatioRepository(_context);
+    }
+
+    private async void AdicionaPatio()
+    {
+        // Arrange
+        var patio = new PatioModel
+        {
+            PatioNome = "Patio 1",
+            PatioVagas = 10,
+        };
+        await _repository.CadastraPatio(patio);
+        await _context.SaveChangesAsync();
+    }  
+    
+    [Fact]
+    public async void BuscaTodosPatios_ComSucesso()
+    {
+        // Arrange
+        AdicionaPatio();
+
+        // Act
+        List<PatioModel> patios = await _repository.RecuperaPatios();
+
+        // Assert
+        Assert.NotNull(patios);
+    } 
+
+    [Fact]
+    public async void BuscaPatioPorId_ComSucesso()
+    {
+        // Arrange
+        AdicionaPatio();
+
+        // Act
+        PatioModel patio = await _repository.RecuperaPatioPorId(1);
+
+        // Assert
+        Assert.Equal("Patio 1", patio.PatioNome);
+    }
+
+    [Fact]
+    public async void AtualizaPatio_ComSucesso()
+    {
+        // Arrange
+        AdicionaPatio();
+        PatioModel patio = await _repository.RecuperaPatioPorId(1);
+        patio.PatioNome = "Patio 2";
+
+        // Act
+        await _repository.AtualizaPatio(patio);
+        PatioModel patioAtualizado = await _repository.RecuperaPatioPorId(1);
+
+        // Assert
+        Assert.Equal("Patio 2", patioAtualizado.PatioNome);
+    }
+
+    [Fact]
+    public async void CadastraPatio_ComSucesso()
+    {
+        // Arrange
+        var patio = new PatioModel
+        {
+            PatioNome = "Patio 1",
+            PatioVagas = 10,
+        };
+
+        // Act
+        await _repository.CadastraPatio(patio);
+        PatioModel patioCadastrado = await _repository.RecuperaPatioPorId(1);
+
+        // Assert
+        Assert.Equal("Patio 1", patioCadastrado.PatioNome);
+    }
+
+    [Fact]
+    public async void DeletaPatio_ComSucesso()
+    {
+        // Arrange
+        AdicionaPatio();
+        PatioModel patio = await _repository.RecuperaPatioPorId(1);
+
+        // Act
+        await _repository.DeletaPatio(patio);
+        PatioModel patioDeletado = await _repository.RecuperaPatioPorId(1);
+
+        // Assert
+        Assert.Null(patioDeletado);
+    }
+}
---
+++ EstacionamentoTest/Repository/RegistroVeiculoRepositoryTest.cs
@@ -0,0 +1,95 @@
+﻿using EstacionamentoV2.Context;
+using EstacionamentoV2.Model;
+using EstacionamentoV2.Repository;
+
+namespace EstacionamentoTest.Repository;
+
+public class RegistroVeiculoRepositoryTest
+{
+    private readonly EstacionamentoContext _context;
+    private readonly RegistroVeiculoRepository _repository;
+
+    public RegistroVeiculoRepositoryTest()
+    {
+        _context = GeradorContextoEmMemoria.GerarContexto();
+        _repository = new RegistroVeiculoRepository(_context);
+    }
+
+    private async void AdicionaVeiculoEstacionado()
+    {
+        // Arrange
+        var registro = new RegistroVeiculoModel
+        {
+            Placa = "ABC1234",
+            DataEntrada = DateTime.Now,
+            Modelo = "Gol",
+            Mensalista = false,
+            PatioID = 1,
+        };
+        await _repository.RegistrarEntradaVeiculo(registro);
+        await _context.SaveChangesAsync();
+    }
+
+    [Fact]
+    public async void BuscaTodosVeiculosEstacionados_ComSucesso()
+    {
+        // Arrange
+        AdicionaVeiculoEstacionado();
+
+        // Act
+        List<RegistroVeiculoModel> veiculosEstacionados = await _repository.ConsultarVeiculosEstacionados();
+
+        // Assert
+        Assert.NotNull(veiculosEstacionados);
+    }
+
+    [Fact]
+    public async void BuscaVeiculoPorPlaca_ComSucesso()
+    {
+        // Arrange
+        AdicionaVeiculoEstacionado();
+
+        // Act
+        RegistroVeiculoModel veiculo = await _repository.ConsultarVeiculoEstacionado("ABC1234");
+
+        // Assert
+        Assert.Equal("ABC1234", veiculo.Placa);
+    }
+
+    [Fact]
+    public async void RegistraEntradaVeiculo_ComSucesso()
+    {
+        // Arrange
+        var registro = new RegistroVeiculoModel
+        {
+            Placa = "ABC1234",
+            DataEntrada = DateTime.Now,
+            Modelo = "Gol",
+            Mensalista = false,
+            PatioID = 1,
+        };
+
+        // Act
+        await _repository.RegistrarEntradaVeiculo(registro);
+
+        // Assert
+        RegistroVeiculoModel veiculo = await _repository.ConsultarVeiculoEstacionado("ABC1234");
+        Assert.NotNull(veiculo);
+    }
+
+    [Fact]
+    public async void RegistraSaidaVeiculo_ComSucesso()
+    {
+        // Arrange
+        AdicionaVeiculoEstacionado();
+        RegistroVeiculoModel veiculo = await _repository.ConsultarVeiculoEstacionado("ABC1234");
+        veiculo.DataSaida = DateTime.Now;
+
+        // Act
+        await _repository.RegistrarSaidaVeiculo(veiculo);
+
+        // Assert
+        RegistroVeiculoModel veiculoSaida = await _repository.ConsultarVeiculoEstacionado("ABC1234");
+        Assert.NotNull(veiculoSaida.DataSaida);
+    }
+}
